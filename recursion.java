000c801ac73126cb12234ba2bdb015407afd542b
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2025-06-12 16:58:39 +0530

    factorial

--- recursion.java
+++ recursion.java
@@ -1,19 +1,18 @@
 import java.util.*;
 public class recursion {
-    public static void printSum(int n, int i, int sum){
-        if(n == i){
-            sum += i;
-            System.out.println(sum);
-            return;
-        }
-        sum += i;
-        printSum(n, i+1, sum);
+public static int calcfactorial(int n){
+    if(n == 1 || n == 0){
+        return 1;
     }
+    int factorialnm1=calcfactorial(n-1);
+    int factorial=n*factorialnm1;
+    return factorial;
+}
     
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        printSum(n, 1, 0);
+        System.out.println(calcfactorial(n));
     }
 }
