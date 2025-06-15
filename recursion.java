9dc61da921651881dd0df079c7619c582292ccdb
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2025-06-15 16:22:03 +0530

    fibonacci

--- recursion.java
+++ recursion.java
@@ -1,18 +1,21 @@
 import java.util.*;
 public class recursion {
-public static int calcfactorial(int n){
-    if(n == 1 || n == 0){
-        return 1;
+    
+    public static void fib(int a, int b, int n){
+        if(n == 0){
+            return;
+        }
+        int c=a+b;
+        System.out.println(c);
+
+        fib(b, c, n-1);
     }
-    int factorialnm1=calcfactorial(n-1);
-    int factorial=n*factorialnm1;
-    return factorial;
-}
-    
-    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(calcfactorial(n));
+        int a=0, b=1;
+        System.out.println(a);
+        System.out.println(b);
+        fib(0, 1, n-2);
     }
 }
