913364a7a19ce5e8fbd11719a57a32438139c833
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2025-06-16 15:41:12 +0530

    string reverse

--- recursion.java
+++ recursion.java
@@ -1,19 +1,19 @@
 import java.util.*;
 public class recursion {
     
-public static void towerOfHanoi(int n, String src, String helper, String dest){
-    if(n == 1){
-            System.out.println("transfer disk "+n+" from "+src+" to "+dest);
-            return;
-    }
-    towerOfHanoi(n-1, src, dest, helper);
-    System.out.println("transfer disk "+n+" from "+src+" to "+dest);
-    towerOfHanoi(n-1, helper, src, dest);
+public static void stringRev(String str, int idx){
+    if(idx == 0){
+        System.out.print(str.charAt(idx));
+        return;
+    }    
+    System.out.print(str.charAt(idx));
+    stringRev(str, idx-1);
 }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        towerOfHanoi(n, "S", "H", "D");
+        String r = "raghav";
+        stringRev(r, r.length()-1);
     }
 }
